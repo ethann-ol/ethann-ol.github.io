6a6991786226c556ed49cd8798be6d910ee15211
drag0nol <ethann.cochenet@gmail.com>
2024-11-30 14:14:14 +0100

    changement mise en page + changement DA

--- script.js
+++ script.js
@@ -11,12 +11,20 @@ let hour = date.getHours();
 
 commoditer = document.querySelector("h1")
 
-if (hour > 6 && hour < 18){
-    commoditer.textContent = "Bonjour, goodMorning";
+if (hour >= 6 && hour > 20 ) {
+    commoditer.textContent = "Qu'est-ce que tu fais là ???/ You don't have to be there at this time !!!";
+}
+else if (hour > 6 && hour < 12)
+{
+    commoditer.textContent = "Bonjour, GoodMorning !";
+}
+else if (hour >= 12 && hour < 18)
+{
+    commoditer.textContent = "Bon après-midi, GoodAfternoon !";
 }
 else
 {
-    commoditer.textContent = "Bonsoir, goodEvening";
+    commoditer.textContent = "Bonsoir, GoodEvening !";
 }
 
 
