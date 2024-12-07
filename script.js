c5fa1431a6c19f53c7dbcbb62dba0ccc8f65911e
drag0nol <ethann.cochenet@gmail.com>
2024-12-07 21:13:54 +0100

    traduction
    ajout de la traduction du site

--- script.js
+++ script.js
@@ -8,28 +8,69 @@ function create(tag, container, text=null){
 }
 const date = new  Date();
 let hour = date.getHours();
+const html = document.querySelector('html')
+let language = html.lang 
+console.log(language)
+ 
+// setInterval(function(){
+//     language = html.lang 
+
+// },10)
 
 commoditer = document.querySelector("h1")
 
-if (hour <= 6 || hour > 20 ) {
-    commoditer.textContent = "You don't have to be there at this time !!!";
-}
-else if (hour > 6 && hour < 12)
-{
-    commoditer.textContent = "Bonjour / GoodMorning !";
-}
-else if (hour >= 12 && hour < 18)
-{
-    commoditer.textContent = "Bon après-midi, GoodAfternoon !";
-}
-else
-{
-    commoditer.textContent = "Bonsoir, GoodEvening !";
-}
+if (language == "en"){
 
+    if (hour <= 6 || hour > 20 ) {
+        commoditer.textContent = "You don't have to be there at this time !!!";
+    }
+    else if (hour > 6 && hour < 12)
+    {
+        commoditer.textContent = "GoodMorning !";
+    }
+    else if (hour >= 12 && hour < 18)
+    {
+        commoditer.textContent = "GoodAfternoon !";
+    }
+    else
+    {
+        commoditer.textContent = "GoodEvening !";
+    }
+}else if (language == "fr"){
+    if (hour <= 6 || hour > 20 ) {
+        commoditer.textContent = "Que fait-tu ici !!!";
+    }
+    else if (hour > 6 && hour < 12)
+    {
+        commoditer.textContent = "Bonjour !";
+    }
+    else if (hour >= 12 && hour < 18)
+    {
+        commoditer.textContent = "Bon après-midi !";
+    }
+    else
+    {
+        commoditer.textContent = "Bonsoir !";
+    }
+}
+            
 const sidebarToggleBtn = document.getElementById('sidebar-toggle');
 const sidebar = document.querySelector('.sidebar');
 
 sidebarToggleBtn.addEventListener('click', () => {
     sidebar.classList.toggle('open');
 });
+
+const links = document.querySelectorAll('.sidebar a');
+links.forEach(link => {
+    link.addEventListener('click', () => {
+        sidebar.classList.remove('open');
+    });
+});
+
+document.querySelector("#en").addEventListener("click", () => {
+    window.location.href = "indexEn.html";
+})
+document.querySelector("#fr").addEventListener("click", () => {
+    window.location.href = "index.html";
+})
